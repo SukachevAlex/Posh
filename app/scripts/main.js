88a29105756b5bda29d0518830871f2b58880978
SukachevAlex <sukachevalexandr@gmail.com>
2018-03-02 21:44:04 +0300

    add section services

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -3,4 +3,27 @@ $( document ).ready(function() {
         $('.hamburger__inner').toggleClass('is-active');
         $('.menu').toggleClass('is-visible');
     });
+
+    $('.tabs__link').click(function () {
+       var tab_id = $(this).attr('href');
+
+       $('.tabs__link').removeClass('tab_active');
+       $('.fragment').removeClass('fragment_active');
+
+       $(this).addClass('tab_active');
+       $(tab_id).addClass('fragment_active');
+    });
+
+
+});
+
+var nav = $('.navigation');
+
+$(window).scroll(function()
+{
+    if (nav.offset().top + nav.height() > $('.header').height())
+        nav.addClass('navigation_dark');
+    else
+        nav.removeClass('navigation_dark');
+
 });
